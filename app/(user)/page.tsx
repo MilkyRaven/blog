57873a6ca5fcb47448cbd9ba15359ad0b2d62491
MilkyRaven <milkykiwidev@gmail.com>
2023-02-20 16:38:42 +0100

    ready to build the blog list component

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,13 +1,39 @@
 import { previewData } from "next/headers";
+import { groq } from "next-sanity";
+import { client } from "../../lib/sanity.client";
+import PreviewSuspense from "../../components/PreviewSuspense";
+import PreviewBlogList from "../../components/PreviewBlogList";
+import BlogList from "../../components/BlogList";
+
+const query = groq`
+*[_type=='post'] {
+    ...,
+    author->,
+    categories[]->
+} | order(createdAt desc)
+`
 
 
-export default function HomePage() {
-    if(previewData()) {
-        return <div>Preview mode</div>
+export default async function HomePage() {
+    if (previewData()) {
+        return (
+            <PreviewSuspense fallback={(
+                <div role="status">
+                    <p className="text-center">
+                        Loading Preview Data...
+                    </p>
+                </div>
+            )}>
+                <PreviewBlogList query={query}/>
+                <h1>In Preview Mode</h1>
+            </PreviewSuspense>)
     }
+
+    const posts = await client.fetch(query)
     return (
         <div>
             <h1 className="text-4xl">Welcome</h1>
+            <BlogList posts={posts}/>
         </div>
     )
 }
