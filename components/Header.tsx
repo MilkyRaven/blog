97b93a1951d8dbb0c8b3ed010a7a9ffa06cec984
MilkyRaven <milkykiwidev@gmail.com>
2023-02-20 14:55:38 +0100

    ready for the banner

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Link from "next/link";
+
+function Header() {
+  return (
+    <header>
+        <div>This is the header</div>
+    </header>
+  )
+}
+
+export default Header
