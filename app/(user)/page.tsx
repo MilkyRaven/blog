61df6bd02778bf02facdc4234e0cda365a3eae1a
MilkyRaven <milkykiwidev@gmail.com>
2023-02-20 15:56:10 +0100

    preview apis ready

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,4 +1,10 @@
+import { previewData } from "next/headers";
+
+
 export default function HomePage() {
+    if(previewData()) {
+        return <div>Preview mode</div>
+    }
     return (
         <div>
             <h1 className="text-4xl">Welcome</h1>
